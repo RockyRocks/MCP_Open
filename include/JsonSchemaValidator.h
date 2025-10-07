09120cee82ccde649d7948c444f1ff0301c4732e
Rakeshkumar Raparla <rakeshkumar.raparla@ubisoft.com>
2025-10-07 12:30:50 +0530

    Updating the paths and fixing a compilation error with error handler

--- include/JsonSchemaValidator.h
+++ include/JsonSchemaValidator.h
@@ -6,10 +6,11 @@
 class JsonSchemaValidator {
 public:
     JsonSchemaValidator() = delete;
-    JsonSchemaValidator(const nlohmann::json& schema);
-    bool validate(const nlohmann::json& data);
+    explicit JsonSchemaValidator(const nlohmann::json& schema);
+    bool validate(const nlohmann::json& data/*, const nlohmann::json_schema::basic_error_handler& error*/);
     std::string getErrorMessage() const;
 private:
     nlohmann::json_schema::json_validator validator;
+    // nlohmann::json_schema::basic_error_handler errorHandler;
     std::string lastError;
 };
--- src/JsonSchemaValidator.cpp
+++ src/JsonSchemaValidator.cpp
@@ -2,9 +2,9 @@
 
 JsonSchemaValidator::JsonSchemaValidator(const nlohmann::json& schema) {
     validator.set_root_schema(schema);
-    validator.set_error_handler([this](const nlohmann::json::json_pointer& ptr, const nlohmann::json& instance){
-        lastError = "Invalid field at: " + ptr.to_string();
-    });
+    // validator.set_error_handler([this](const nlohmann::json::json_pointer& ptr, const nlohmann::json& instance){
+    //     lastError = "Invalid field at: " + ptr.to_string();
+    // });
 }
 
 bool JsonSchemaValidator::validate(const nlohmann::json& data) {
